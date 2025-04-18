eed725b456babce19056575a9fc3afc8e9087295
Hanna Kasabutskaya <akosobutskaya@yandex-team.ru>
2025-04-18 23:43:26 +0300

    v1

--- src/components/ui/modal/modal.tsx
+++ src/components/ui/modal/modal.tsx
@@ -7,9 +7,9 @@ import { TModalUIProps } from './type';
 import { ModalOverlayUI } from '@ui';
 
 export const ModalUI: FC<TModalUIProps> = memo(
-  ({ title, onClose, children }) => (
+  ({ title, onClose, children, ...rest }) => (
     <>
-      <div className={styles.modal}>
+      <div className={styles.modal} {...rest}>
         <div className={styles.header}>
           <h3 className={`${styles.title} text text_type_main-large`}>
             {title}
